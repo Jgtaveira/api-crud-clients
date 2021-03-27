e96f88b8553e66d2a521294c8aec518a066a0e5f
Joao Taveira <jgsart22@gmail.com>
2021-03-27 01:29:58 -0300

    Seeding and Clients Project completed

--- api_crud_clients/src/main/resources/data.sql
+++ api_crud_clients/src/main/resources/data.sql
@@ -1,4 +1,12 @@
 INSERT INTO tb_client (name, birth_date, children, cpf, income) VALUES ('João Paulo',TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', 2, '09931144478' , 1500.00);
 INSERT INTO tb_client (name, birth_date, children, cpf, income) VALUES ('Márcia Silva',TIMESTAMP WITH TIME ZONE '2020-08-13T20:50:07.12345Z', 3, '09975444858',2000.00);
 INSERT INTO tb_client (name, birth_date, children, cpf, income) VALUES ('Felipe Jose',TIMESTAMP WITH TIME ZONE '2020-09-13T20:50:07.12345Z', 2, '09615618587', 2300.00);
-INSERT INTO tb_client (name, birth_date, children, cpf, income) VALUES ('Henrique Martins',TIMESTAMP WITH TIME ZONE '2020-10-13T20:50:07.12345Z', 4,'09615618587',2700.00);
+INSERT INTO tb_client (name, birth_date, children, cpf, income) VALUES ('Mathias Rodrigues',TIMESTAMP WITH TIME ZONE '2020-10-13T20:50:07.12345Z', 4,'09615618587',2700.00);
+INSERT INTO tb_client (name, birth_date, children, cpf, income) VALUES ('Evelin Antunes',TIMESTAMP WITH TIME ZONE '2020-02-13T20:50:07.12345Z', 2, '09931144478' , 6500.00);
+INSERT INTO tb_client (name, birth_date, children, cpf, income) VALUES ('Rita de Cassia',TIMESTAMP WITH TIME ZONE '2020-03-13T20:50:07.12345Z', 3, '25875444858',2020.00);
+INSERT INTO tb_client (name, birth_date, children, cpf, income) VALUES ('Antonio Almeida',TIMESTAMP WITH TIME ZONE '2020-04-13T20:50:07.12345Z', 2, '56815618587', 2600.00);
+INSERT INTO tb_client (name, birth_date, children, cpf, income) VALUES ('Michele da Costa',TIMESTAMP WITH TIME ZONE '2020-11-13T20:50:07.12345Z', 4,'09615618587',3700.00);
+INSERT INTO tb_client (name, birth_date, children, cpf, income) VALUES ('Eric Fernandes',TIMESTAMP WITH TIME ZONE '2020-12-13T20:50:07.12345Z', 2, '87931144478' , 1500.00);
+INSERT INTO tb_client (name, birth_date, children, cpf, income) VALUES ('Pedro Sardinha',TIMESTAMP WITH TIME ZONE '2020-03-13T20:50:07.12345Z', 3, '23575444858',2170.00);
+INSERT INTO tb_client (name, birth_date, children, cpf, income) VALUES ('Felipe Alves',TIMESTAMP WITH TIME ZONE '2020-04-13T20:50:07.12345Z', 2, '25815618587', 3200.00);
+INSERT INTO tb_client (name, birth_date, children, cpf, income) VALUES ('Diogo Cunha',TIMESTAMP WITH TIME ZONE '2020-06-13T20:50:07.12345Z', 4,'14715618587',4700.00);
